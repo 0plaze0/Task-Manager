e59ed9f30bf4795b2e52339f01edeefece0ac31c
plaze <banikakash131@gamil.com>
2023-08-08 22:19:00 +0530

    CRUD operation

--- middleware/taskController.js
+++ middleware/taskController.js
@@ -1,12 +1,32 @@
-const getAllData = (req, res) => {
-  res.send("all files send");
+const Task = require("../model/TaskSchema");
+
+const getAllData = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const createTask = (req, res) => {
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task); //ok
+  } catch (err) {
+    res.status(500).json({ msg: err }); //server Error
+  }
 };
-const getTask = (req, res) => {
-  res.send("get task");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task)
+      return res.status(404).json({ msg: `No task with id ${taskID}` });
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update task");
